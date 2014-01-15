f0ac11370020193e7277ee5003efb09179c9f8dc
wtl <wang@wang-Rev-1-0.(none)>
2014-01-15 23:55:32 +0800

    +ALARM DBG OK

--- signal-test.cpp
+++ signal-test.cpp
@@ -1,7 +1,15 @@
 #include<signal.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
 #include<fcntl.h>
-#include<epoll.h>
-#include<socket.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/epoll.h>
 #include<errno.h>
 #define EVENT_MAX_NUMBER 1024
 static int pipefd[2];
@@ -14,10 +22,10 @@ void setnonblocking(int fd)
 void addfd(int epollfd,int fd)
 {
 	epoll_event event;
-	event.data->fields=fd;
+	event.data.fd=fd;
 	event.events=EPOLLIN | EPOLLET;
 	epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event);
-
+	setnonblocking(fd);
 }
 /*信号处理函数*/
 void sig_handle(int sig)
@@ -33,16 +41,17 @@ void sig_handle(int sig)
 void addsig(int sig)
 {
 	struct sigaction sa;
-	memset(sa,'\0',sizeof(sa));
-	sa.sa_handler=sig_handle;
+	memset(&sa,'\0',sizeof(sa));
+	sa.sa_handler=sig_handle; 
 	sa.sa_flags |=SA_RESTART;
 	sigfillset(&sa.sa_mask);
+	assert(sigaction(sig,&sa,NULL)!=-1);
 }
 int main(int argc ,char* argv[])
 {
   if(argc<=2)
   {
-	  printf("%s ip_address port_number\n");
+	  printf("%s ip_address port_number\n",argv[0]);
 	  return 0;
   }
   char* ip_address=argv[1];
@@ -51,7 +60,7 @@ int main(int argc ,char* argv[])
   bzero(&address,sizeof(address));
   address.sin_family=AF_INET;
   address.sin_port=htons(port);
-  inet_pton(AF_INET,ip_address,address.sin_addr);
+  inet_pton(AF_INET,ip_address,&address.sin_addr);
 
   int listenfd=socket(PF_INET,SOCK_STREAM,0);
   assert(listenfd>=0);
@@ -81,28 +90,30 @@ int main(int argc ,char* argv[])
   addsig(SIGCHLD);
   addsig(SIGTERM);
   addsig(SIGINT);
+  addsig(SIGALRM);
   bool stop_server=false;
+  alarm(5);
   while(!stop_server)
   {
-	  int number=epoll_wait(epollfd,events,EVENT_MAX_NUMBER,0);
+	  int number=epoll_wait(epollfd,events,EVENT_MAX_NUMBER,-1);
 	  if(number<0 && errno!=EINTR)
 	  {
 		  printf("epoll_wait errno:%d\n",errno);
 		  return 1;
 	  }
-	  for(int i=0;i<number;i++;)
+	  for(int i=0;i<number;i++)
 	  {
 		 int socketfd=events[i].data.fd;
 		 /*如果就绪的文件描述符是listenfd，则处理新的连接*/
 		 if(socketfd==listenfd)
 		 {
 			 struct sockaddr_in client_address;
-			 int client_addrlen=sizeof(client_address);
-			 int connfd=accept(socketfd,(struct sockaddr*)&client_address,sizeof(client_addrlen));
+			 socklen_t client_addrlen=sizeof(client_address);
+			 int connfd=accept(socketfd,(struct sockaddr*)&client_address,&client_addrlen);
 			 addfd(epollfd,connfd);
 		 }
 		 /*如果是pipefd[0]则处理信号*/
-		 else if(socketfd==pipefd[0] && events.events & EPOLLIN)
+		 else if(socketfd==pipefd[0] && events[i].events & EPOLLIN)
 		 {
 			 char signals[1024];
 			 ret=recv(pipefd[0],signals,sizeof(signals),0);
@@ -132,6 +143,10 @@ int main(int argc ,char* argv[])
 					 case SIGINT:
 						 stop_server=true;
 						 break;
+					 case SIGALRM:
+						alarm(5);
+						 printf("SIGALRM\n");
+						 break;
 					 }
 				 }
 			 }
@@ -140,12 +155,11 @@ int main(int argc ,char* argv[])
 		 {
 		 }
 	  }
-	  printf("close fds\n");
-	  close(listenfd);
-	  close(pipefd[0]);
-	  close(pipefd[1]);
-	  return 0;
   }
+  printf("close fds\n");
+  close(listenfd);
+  close(pipefd[0]);
+  close(pipefd[1]);
   return 0;
 
 }
